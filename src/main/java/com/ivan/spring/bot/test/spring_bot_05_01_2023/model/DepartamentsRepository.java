c4d168997a713c70b8008fe117c3a33105ff94f2
Ivan <maus891@yandex.ru>
2023-01-15 12:54:03 +0300

    + department

---
+++ src/main/java/com/ivan/spring/bot/test/spring_bot_05_01_2023/model/DepartamentsRepository.java
@@ -0,0 +1,6 @@
+package com.ivan.spring.bot.test.spring_bot_05_01_2023.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DepartamentsRepository extends CrudRepository <Departments, Long> {
+}
---
+++ src/main/java/com/ivan/spring/bot/test/spring_bot_05_01_2023/model/Departments.java
@@ -0,0 +1,91 @@
+package com.ivan.spring.bot.test.spring_bot_05_01_2023.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+public class Departments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String departmentName;
+    @Column(name = "max_salary")
+    private int maxSalary;
+    @Column(name = "min_salary")
+    private int minSalary;
+
+    public Departments() {
+    }
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "departments", fetch = FetchType.EAGER)
+    private List<User> userList;
+
+    public Departments(String departmentName, int maxSalary, int minSalary) {
+        this.departmentName = departmentName;
+        this.maxSalary = maxSalary;
+        this.minSalary = minSalary;
+    }
+
+    public void addUserToDepartments(User user){
+        if (userList == null){
+            userList = new ArrayList<>();
+        } else {
+            userList.add(user);
+            user.setDepartments(this);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @Override
+    public String toString() {
+        return "Departments{" +
+                "id=" + id +
+                ", departmentName='" + departmentName + '\'' +
+                ", maxSalary=" + maxSalary +
+                ", minSalary=" + minSalary +
+                '}';
+    }
+}
